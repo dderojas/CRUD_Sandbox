440af1cf7b154422d74133dbd3df002cda3a59f9
Don de Rojas <derojasdonny@gmail.com>
2018-10-10 12:48:08 -0700

    started implementing sequelize and installed mysql2

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('database','username','password', {
+const sequelize = new Sequelize('donDataBase','root','', {
   host: 'localhost',
+  port: 3306,
   dialect: 'mysql',
   operatorsAliases: false,
   pool: {
@@ -9,7 +10,30 @@ const sequelize = new Sequelize('database','username','password', {
     acquire: 30000,
     idle: 10000
   }
-})
+});
+
+const User = sequelize.define('user', {
+  newColumn: {
+    type: Sequelize.STRING
+  },
+  secondColumn: {
+    type: Sequelize.STRING
+  }
+});
+
+// force: true will drop the table if it already exists
+User.sync({force: true}).then(() => {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
+
+User.findAll()
+.then(users => {
+  console.log(users, 'we did it!');
+});
 
 sequelize
 .authenticate()
@@ -18,4 +42,6 @@ sequelize
 })
 .catch((err) => {
   console.log(`can't connect to database`, err);
-})
+});
+
+module.exports.User = User;
